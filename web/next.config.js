bbae74a2a1b757e91fca4e3073d82e8d2d47b04e
Bijan Kazemi-Shirkadeh <bkazemi@users.sf.net>
2026-02-06 21:52:26 -0800

    feat: allow iframe embedding from personal sites

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://b.shirkadeh.org https://shirkadeh.org https://bkazemi.github.io",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
